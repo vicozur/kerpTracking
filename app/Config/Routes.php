511e9385d53e34d45478429d59b7d50f7a86a6a3
Victor Zurita <vico.zur@gmail.com>
2026-02-09 11:21:28 -0400

    update routes system

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+$routes->get('/', 'LoginController::index');
 $routes->get('login', 'LoginController::index');
 $routes->post('login/authentication', 'LoginController::authentication');
 $routes->get('login/googleAuth', 'LoginController::googleAuth');
